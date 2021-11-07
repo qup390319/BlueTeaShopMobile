4292feb8b9996ccfe3055355d0bea2bdd19943e4
yoyo <ya90312@gmail.com>
2021-11-08 00:32:08 +0800

    use OKhttp to get teas Table

--- app/src/main/java/com/example/blueteashopmobile/OrderFragment.java
+++ app/src/main/java/com/example/blueteashopmobile/OrderFragment.java
@@ -2,11 +2,25 @@ package com.example.blueteashopmobile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +69,36 @@ public class OrderFragment extends Fragment {
         }
     }
 
+    private class Data {
+        String id;
+        String item;
+        int price;
+        String size;
+        String type;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        String url = "https://a66a-2001-b011-b800-9666-a5b6-e17-6e1-3f0c.ngrok.io/api/teas";
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder().url(url).build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String result = response.body().string();
+                Log.d("tickets",result);
+                List<Data> list = new Gson().fromJson(result, new TypeToken<List<Data>>() {
+                }.getType());
+            }
+        });
         return inflater.inflate(R.layout.fragment_order, container, false);
     }
+
 }
